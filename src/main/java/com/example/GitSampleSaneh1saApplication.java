8639495b3d7b3efdaeaf655383fefa8a859966c6
saneh1sa <yuta.sanehisa@rakus.co.jp>
2021-05-20 12:22:18 +0900

    最初のコミットです。

---
+++ src/main/java/com/example/GitSampleSaneh1saApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleSaneh1saApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleSaneh1saApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GitSampleSaneh1saApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSampleSaneh1saApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
