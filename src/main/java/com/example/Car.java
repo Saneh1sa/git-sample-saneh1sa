ef8985d7e8fc06fe61bb77649d923db0d37383e8
saneh1sa <yuta.sanehisa@rakus.co.jp>
2021-05-20 14:13:31 +0900

    コミットを統合させました。
    2番目のコミットです。
    
    最後のコミットです。

--- src/main/java/com/example/Car.java
+++ src/main/java/com/example/Car.java
@@ -5,6 +5,8 @@ public class Car {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Carが存在しています。ぶ～ん。");
+		System.out.println("Carが存在しています。ぶ～ん。");
+		System.out.println("Carが存在しています。ぶ～ん。");
 	}
 
 }
